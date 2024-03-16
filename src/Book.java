e1532b55c2b6b55220a9a5d5a22267ec73b9eca3
Bell <belalofficial123@gmail.com>
2024-03-16 16:42:16 +0200

    Commit please

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,65 @@
 public class Book {
+    private String title;
+    private String author;
+    private int year;
+    private int pages;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+    public Book(String title, String author, int year) {
+        this(title, author); // this calls the constructor with 2 parameters (title and author)
+        this.year = year;
+    }
+    public Book(String title, String author, int year, int pages) {
+        this(title, author, year); // this calls the constructor with 3 parameters (title, author and year)
+        this.pages = pages;
+    }
+
+
+    // Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                ", pages=" + pages +
+                '}';
+    }
+
 }
--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,42 @@
 
 public class Employee {
+    // Final fields are initialized only once, either via an initializer or an assignment statement.
+    private final String name;
+    private final String position;
+    private final int year;
+    // Constructors
+    public Employee(String name, String position, int year) {
+        this.name = name;
+        this.position = position;
+        this.year = year;
+    }
+
+    public Employee(String name, String position) {
+        this(name, position, 0); // this calls the constructor with 3 parameters (name, position and year)
+    }
+
+    public Employee(String name) {
+        this(name, "Default"); // this calls the constructor with 2 parameters (name and position)
+    }
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                ", year=" + year +
+                '}';
+    }
 }
