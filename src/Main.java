738466977ae79d8a053ac93a8d8101d15f97b034
Bell <belalofficial123@gmail.com>
2024-03-16 12:08:52 +0200

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        
+        // update this shit
+        int x;
+        String str;
+
+        Scanner scanner = new Scanner(System.in);
+        str = scanner.next();
+        x = Integer.parseInt(str);
+        System.out.println(x);
     }
 }
